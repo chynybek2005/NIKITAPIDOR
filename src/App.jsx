5c3d5ab60041be76de0c31a0d1a23ecfe69d024b
chynybek2005 <mister.4ika@gmail.com>
2023-04-28 12:42:13 +0900

    nikita beri blyat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Karim from './components/Karim';
 
 function App() {
   return (
     <div className="App">
-      
+      <Karim/>
     </div>
   );
 }
---
+++ src/components/Karim.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Karim = () => {
+
+    const GoldCardState = useSelector((state) => state)
+    const dispatch = useDispatch()    
+    return (
+        <div>
+            <h1>{GoldCardState.user}</h1>
+            <h1>{GoldCardState.cash}</h1>
+
+
+            <button onClick={() => dispatch({type: "ADD CASH", payload: 5000})}>+ cash</button>
+        </div>
+    );
+};
+
+export default Karim;
